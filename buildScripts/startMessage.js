33a85def1ddf93470aec273d7c6e94197670011f
mohitpundir <mohit@aquacrmsoftware.com>
2018-08-19 19:59:06 +0530

    added eslint to check for errors and warning

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 //var chalk = require('chalk');
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting up in Dev. mode...'));
+console.log(chalk.green('Starting up in Dev. mode...')); // eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 //debugger;
-console.log(`I would pay ${courseValue} for this aweseome content`);
+console.log(`I would pay ${courseValue} for this aweseome content`); // eslint-disable-line no-console
