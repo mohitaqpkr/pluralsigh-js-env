b7e7c2c5f08561122cf3a278099fc6d3ed7e6acb
mohitpundir <mohit@aquacrmsoftware.com>
2018-08-20 14:33:57 +0530

    added table and test json data

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -26,6 +26,16 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+
+app.get('/users',function(req,res){
+  //hard coding for simplicity sake. Pretend this hits a real dataabse
+  res.json([
+    {"id": 1, "firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2, "firstName":"Arthur","lastName":"Ronald","email":"arthur@gmail.com"},
+    {"id": 3, "firstName":"Krish","lastName":"John","email":"krish@gmail.com"}
+  ]);
+});
+
 app.listen(port,function(err){
   if(err){
     console.log(err);
